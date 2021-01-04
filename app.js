e63e3a7b699c95a6cd91b84b377a21146a107cde
melos <melosdauti7@gmail.com>
2021-01-04 14:25:22 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+var svg = d3.select("svg");
+
+var rect = svg
+  .append("rect")
+  .attr("height", 10)
+  .attr("width", 10)
+  .style("fill", "red");
+
+rect.style("fill", "purple");
+// d3.select("rect").style("fill", "green");
